a919ecd02acea2336af580773a68fac9880f65a9
Gian <gdass8000@gmail.com>
2022-05-08 17:17:49 -0300

    Dia 4 (Mobile)

---
+++ mobile/src/components/Form/index.tsx
@@ -0,0 +1,109 @@
+import { ArrowLeft } from 'phosphor-react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Image, TouchableOpacity } from 'react-native';
+import { theme } from '../../theme';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+import { Button } from '../Button';
+import { ScreenshotButton } from '../ScreenshotButton';
+import { FeedbackType } from '../Widget';
+import { styles } from './styles';
+import { captureScreen } from 'react-native-view-shot';
+import { api } from '../../libs/api';
+import * as FileSystem from 'expo-file-system';
+
+interface Props {
+    feedbackType: FeedbackType;
+    onFeedbackCanceled: () => void;
+    onFeedbackSent: () => void;
+}
+
+export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props) {
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+
+    const [screenshot, setScreenshot] = useState<string | null>(null);
+
+    const [comment, setComment] = useState('');
+
+    const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+    function handleScreenshot() {
+        captureScreen({
+            format: 'jpg',
+            quality: 0.8
+        })
+            .then(uri => setScreenshot(uri))
+            .catch(err => console.error(err));
+    }
+    function handleScreenshotRemove() {
+        setScreenshot(null);
+    }
+
+    async function handleSendFeedback() {
+        if (isSendingFeedback) {
+            return
+        }
+
+        setIsSendingFeedback(true);
+
+        const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' });
+
+        try {
+            console.log('Sending feedback...');
+
+            await api.post('/feedbacks', {
+                type: feedbackType,
+                comment,
+                screenshot: `data:image/jpg;base64, ${screenshotBase64}`
+            })
+
+            onFeedbackSent();
+        } catch (error) {
+            console.error(error);
+            setIsSendingFeedback(false);
+        }
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <TouchableOpacity
+                    onPress={onFeedbackCanceled}
+                >
+                    <ArrowLeft
+                        size={24}
+                        weight="bold"
+                        color={theme.colors.text_secondary}
+
+                    />
+                </TouchableOpacity>
+                <View style={styles.titleContainer}>
+                    <Image
+                        source={feedbackTypeInfo.image}
+                        style={styles.image}
+                    />
+                    <Text style={styles.titleText}>
+                        {feedbackTypeInfo.title}
+                    </Text>
+                </View>
+            </View>
+            <TextInput
+                multiline
+                style={styles.input}
+                placeholder="Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo..."
+                placeholderTextColor={theme.colors.text_secondary}
+                onChangeText={setComment}
+            />
+            <View style={styles.footer}>
+                <ScreenshotButton
+                    onTakeShot={handleScreenshot}
+                    onRemoveShot={handleScreenshotRemove}
+                    screenshot={screenshot}
+                />
+                <Button
+                    onPress={handleSendFeedback}
+                    isLoading={isSendingFeedback}
+                />
+            </View>
+        </View>
+    );
+}
